4dfa8f6cfd1510d0b57552e17f28ab231045a3bf
EdwinMorales <edwinmorales6596@gmail.com>
2026-05-04 23:08:23 -0600

    otra prueba de commit

--- app/src/main/java/com/example/kash/MainActivity.kt
+++ app/src/main/java/com/example/kash/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     KashTheme {
-        Greeting("cesar comita")
+        Greeting("el mero edwin commiddd comita")
     }
 }
