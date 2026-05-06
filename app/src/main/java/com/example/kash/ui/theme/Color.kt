8422bc0a6e0ff85fc796fee86c3150db6ddb4701
EdwinMorales <edwinmorales6596@gmail.com>
2026-05-05 19:49:34 -0600

    inicio de login

--- app/src/main/java/com/example/kash/ui/theme/Color.kt
+++ app/src/main/java/com/example/kash/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val EmeraldPrimary = Color(0xFF00897B) // El verde oscuro del fondo
+val EmeraldLight = Color(0xFF4DB6AC)    // Un verde más claro para detalles
+val CardWhite = Color(0xFFFFFFFF)       // El blanco de la tarjeta de login
