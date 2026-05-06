411a59038e30d69d61c7b15a11a3d97cca0f5586
cesarin <Bu244100070@uma.edu.sv>
2026-05-05 20:11:41 -0600

    Intento de hacer roomapp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
-    alias(libs.plugins.android.application)
+
+        alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-}
+
+    }
+
 
 android {
     namespace = "com.example.kash"
@@ -48,6 +51,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(platform(libs.androidx.compose.bom))
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -55,4 +60,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+
+
 }
